531b92afac17471d02131f0ed75bc0a76eb14e4a
Tamojay Dey <tamojay@interpl.ai>
2023-09-07 12:57:47 +0530

    Initial commit

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Footer() {
-    return (
-        <footer>
-            <Container>
-                <Row>
-                    <Col className="text-center py-3">Copyright &copy; ProShop</Col>
-                </Row>
-            </Container>
-        </footer>
-    )
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">
+            Copyright &copy; HappyShop |{" "}
+            <a href="https://linkedin.com/in/tamojay">Tamojay Dey</a>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
